8296e50ba9582957c4f1eb88aa58bff5f8c8d63a
Ahsan Ali <ahsanalimemon2002@gmail.com>
2025-04-15 04:47:07 +0500

    nltk

--- myApp.py
+++ myApp.py
@@ -1,3 +1,18 @@
+import nltk
+import os
+
+# Permanent NLTK data solution for Streamlit Cloud
+nltk_data_path = os.path.join(os.getcwd(), "nltk_data")
+os.makedirs(nltk_data_path, exist_ok=True)
+nltk.data.path.append(nltk_data_path)
+
+# Download required NLTK data if missing
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt', download_dir=nltk_data_path)
+    nltk.download('punkt_tab', download_dir=nltk_data_path)  # Add this line
+    nltk.download('wordnet', download_dir=nltk_data_path)   # For lemmatization
 import streamlit as st
 from computeSimilarity import rankDocs, loadTfidfData
 
